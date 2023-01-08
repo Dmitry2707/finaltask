990553ab1a8852a59da1289108d7e8a05795a97a
Dmitry2707 <kemper1495@gmail.com>
2023-01-08 12:41:00 +0300

    commit2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿string[] array1 = new string[6] {"123", "23", "321", "rm", "mc","555"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
 
